c0021dcdaed7ac1b9c9f19888ecac043166d65a5
Greg Vondra <gvondra@gmail.com>
2016-08-13 10:20:43 -0500

    added insert sp for projects; added project member table

---
+++ Database/Definition/Database/tnyk/Stored Procedures/ISP_Project.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [tnyk].[ISP_Project]
+	@id int out,
+	@ownerId int,
+	@title NVARCHAR(500)
+AS
+BEGIN
+	INSERT INTO [tnyk].[Project] ([OwnerId], [Title])
+	VALUES (@ownerId, @title)
+	;
+	SET @id = SCOPE_IDENTITY();
+END
---
+++ Database/Definition/Database/tnyk/Tables/ProjectMember.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [tnyk].[ProjectMember]
+(
+	[ProjectMemberId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [ProjectId]       INT NOT NULL,
+	[EmailAddress]    NVARCHAR (250) NOT NULL, 
+    CONSTRAINT [FK_ProjectMember_To_Project] FOREIGN KEY ([ProjectId]) REFERENCES [tnyk].[Project]([ProjectId]),
+)
